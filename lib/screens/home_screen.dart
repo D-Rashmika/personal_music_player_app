552b5d11d072b9fab4a131bc1d9cf5f477846f01
D-Rashmika <dilarashmika@gmail.com>
2024-12-27 16:45:07 +0530

    Add const constructor to HomeScreen and update UI elements for improved styling

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -1,14 +1,19 @@
 import 'package:flutter/material.dart';
 
 class HomeScreen extends StatelessWidget {
+  const HomeScreen({super.key});
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      backgroundColor: const Color.fromARGB(255, 0, 0, 0),
       appBar: AppBar(
-        title: Text('Home Screen'),
+        backgroundColor: const Color.fromARGB(255, 28, 25, 25),
+        title: Text('Rylix', style: TextStyle(color: Colors.white)),
+        centerTitle: true,
       ),
       body: Center(
-        child: Text('Welcome to the Home Screen!'),
+        
       ),
     );
   }
