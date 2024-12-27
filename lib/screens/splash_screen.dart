fd00c8105bf2f811b492951268dd8c1bbf8c4910
D-Rashmika <dilarashmika@gmail.com>
2024-12-27 15:33:39 +0530

    Add blinking effect to music note icon on splash screen

--- lib/screens/splash_screen.dart
+++ lib/screens/splash_screen.dart
@@ -1,37 +1,67 @@
 import 'package:flutter/material.dart';
-import 'package:flutter/cupertino.dart';
 import 'dart:async';
 
-class SplashScreen extends StatefulWidget{
+class SplashScreen extends StatefulWidget {
   @override
   _SplashScreenState createState() => _SplashScreenState();
 }
 
-class _SplashScreenState extends State<SplashScreen>{
+class _SplashScreenState extends State<SplashScreen> {
+  bool _isVisible = true; // Visibility flag for the music note icon
+  int _blinkCount = 0; // To track how many times the icon has blinked
+
   @override
-  void initState(){
+  void initState() {
     super.initState();
-    Timer(const Duration(seconds: 3), (){
-      Navigator.pushReplacementNamed(context, '/home');
+
+    // Timer to handle the blinking effect
+    Timer.periodic(Duration(seconds: 1), (timer) { // Slow down the blink
+      if (_blinkCount < 3) { // Blink 3 times
+        setState(() {
+          _isVisible = !_isVisible; // Toggle visibility
+          _blinkCount++;
+        });
+      } else {
+        timer.cancel(); // Stop blinking after 3 times
+        // Navigate to home screen after the animation
+        Future.delayed(Duration(seconds: 1), () {
+          Navigator.pushReplacementNamed(context, '/home');
+        });
+      }
     });
   }
 
   @override
-  Widget build(BuildContext context){
+  Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: Colors.black,
-      body: Center(
-        child: AnimatedOpacity(
-          opacity: 1.0,
-          duration: Duration(microseconds: 500),
-          child: Icon(
-            Icons.music_note,
-            color: Colors.red,
-            size: 100.0,
+      backgroundColor: Colors.black,  // Black background
+      body: Stack(
+        children: [
+          Center(
+            child: AnimatedOpacity(
+              opacity: _isVisible ? 1.0 : 0.0, // Make the icon blink
+              duration: Duration(seconds: 1), // Slow down the blink animation
+              child: Icon(
+                Icons.music_note, // Music note icon
+                color: Colors.red, // Red color for the music note
+                size: 55.0, // Size of the music note
+              ),
+            ),
           ),
-         
-          
-        ),
+          Positioned(
+            bottom: 20.0,
+            left: 0.0,
+            right: 0.0,
+            child: Text(
+              "Powered by Spotify",
+              textAlign: TextAlign.center,
+              style: TextStyle(
+                color: Colors.red,
+                fontSize: 16.0,
+              ),
+            ),
+          ),
+        ],
       ),
     );
   }
